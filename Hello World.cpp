68dfdd65422302ce6f094228c8341fd211a2e1b1
Uglaos <42736734+Uglaos@users.noreply.github.com>
2018-11-27 09:35:42 +0100

    text
    added include pch.h
    added change

--- Hello World.cpp
+++ Hello World.cpp
@@ -1,10 +1,12 @@
 #include <iostream> // standard input output
+#include "pch.h"
 
 using namespace std; // standard name (mine or standard)
 
 int main() // function
 {
 	cout << "Hello World!" << endl; // console log Hello World
+	cout << "change";
 	system ("PAUSE"); // pause first and then cout
 	return 0; // return no error
 }
