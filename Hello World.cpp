10db68224f2949e14993ca08232681bb70cc86d4
Uglaos <42736734+Uglaos@users.noreply.github.com>
2018-11-27 09:29:24 +0100

    text
    added first file

---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // standard input output
+
+using namespace std; // standard name (mine or standard)
+
+int main() // function
+{
+	cout << "Hello World!" << endl; // console log Hello World
+	system ("PAUSE"); // pause first and then cout
+	return 0; // return no error
+}
